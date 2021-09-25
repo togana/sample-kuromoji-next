a16aa57a42880dd56a0dd0a49dd389d6b71fb01b
togana <o.togana@gmail.com>
2021-09-25 14:47:50 +0900

    kuromojin を利用して読みがな変換できないか検証

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { getTokenizer } from "kuromojin"
+
+getTokenizer({dicPath: "/dict"})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
