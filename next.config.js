58cdc1ba0973fe5092b21630c57008c70154a7dc
togana <o.togana@gmail.com>
2021-09-27 09:52:04 +0900

    gh-pages用にasset配下の変更

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const debug = process.env.NODE_ENV !== 'production'
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: !debug ? '/sample-kuromoji-next/' : '',
 }
